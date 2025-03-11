70f3c4010af640b26dce08f63528c1f1329ee7be
Cyril <multya77@gmail.com>
2025-03-11 22:15:23 +0800

    add read file

--- src/graph_reader.rs
+++ src/graph_reader.rs
@@ -1,27 +1,51 @@
-use std::{fs::File, io::{BufRead, BufReader}};
 use petgraph::graph::UnGraph;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 use thiserror::Error;
 
-#[derive(Error,Debug)]
+#[derive(Error, Debug)]
 pub enum GraphErr {
-    #[error("Invalid DIMACS format:{0}")]
+    #[error("invalid DIMACS format: {0}")]
     ParseError(String),
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
 }
 
-pub fn read_dimacs(path:&str)-> Result<UnGraph<(),()>,GraphErr>{
-    let file = File::open(path).map_err(|e| GraphErr::ParseError(e.to_string()))?;
+impl From<&str> for GraphErr {
+    fn from(s: &str) -> Self {
+        GraphErr::ParseError(s.into())
+    }
+}
+
+pub fn read_dimacs(path: &str) -> Result<UnGraph<(), ()>, GraphErr> {
+    let file = File::open(path)?;
     let reader = BufReader::new(file);
     let mut graph = UnGraph::new_undirected();
-    let mut node = Vec::new();
+    let mut nodes = Vec::new();
 
-    for line in reader.lines(){
-        let line = line.map_err(|e| GraphErr::ParseError(e.to_string()))?;
-        if line.starts_with('e'){
-            let stuff:Vec<&str> = line.split_ascii_whitespace().collect();
-            if stuff.len()!=3{
+    for line in reader.lines() {
+        let line = line?;
+        // e 10 2
+        if line.starts_with('e') {
+            let stuff: Vec<&str> = line.split_ascii_whitespace().collect();
+            if stuff.len() != 3 {
                 return Err(GraphErr::ParseError("bad edge format!".into()));
             }
+            let u: u32 = stuff[1].parse().unwrap();
+            let v: u32 = stuff[2].parse().unwrap();
+            let u_idx = *nodes.get(u as usize - 1).unwrap();
+            let v_ind = *nodes.get(v as usize - 1).unwrap();
+            graph.add_edge(u_idx, v_ind, ());
+            continue;
+        }
+        // p edge 200 13089
+        if line.starts_with('p') {
+            let stuff: Vec<&str> = line.split_ascii_whitespace().collect();
+            let node_count: usize = stuff[2].parse().unwrap();
+            nodes = (0..node_count).map(|_| graph.add_node(())).collect();
         }
     }
-    todo!()
+    Ok(graph)
 }
--- tests/dimacs_test.rs
+++ tests/dimacs_test.rs
@@ -1,11 +1,19 @@
 #[cfg(test)]
 mod tests {
-    use super::*;
-    
+    use clique::graph_reader::read_dimacs;
+
+    #[test]
+    fn test_read(){
+        // p edge 200 13089
+        let graph = read_dimacs("data/brock200_4.clq").unwrap();
+        assert_eq!(graph.node_count(),200);
+        assert_eq!(graph.edge_count(),13089);
+    }
+
     #[test]
     fn test_brock200_4() {
         let graph = read_dimacs("data/brock200_4.clq").unwrap();
-        let clique = bron_kerbosch(&graph);
-        assert!(clique.len(), 17);
+        // let clique = bron_kerbosch(&graph);
+        // assert!(clique.len(), 17);
     }
 }
