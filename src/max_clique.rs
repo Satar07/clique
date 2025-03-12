d75a62e794355a434a02a888c37eff58dd6703c5
Cyril <multya77@gmail.com>
2025-03-12 22:57:44 +0800

    level up!

--- src/max_clique.rs
+++ src/max_clique.rs
@@ -1,11 +1,16 @@
-use std::collections::HashSet;
+use std::{cmp::Reverse, collections::{HashMap, HashSet}};
 
 use petgraph::graph::{NodeIndex, UnGraph};
 
 pub fn bron_kerbosch(graph: &UnGraph<(), ()>) -> Vec<NodeIndex> {
     let mut result = HashSet::new();
+    let neighbors_map = graph
+        .node_indices()
+        .map(|n| (n, graph.neighbors(n).collect()))
+        .collect();
     bk_dfs(
         graph,
+        &neighbors_map,
         &mut HashSet::new(),
         &mut graph.node_indices().collect(),
         &mut HashSet::new(),
@@ -16,6 +21,7 @@ pub fn bron_kerbosch(graph: &UnGraph<(), ()>) -> Vec<NodeIndex> {
 
 fn bk_dfs(
     graph: &UnGraph<(), ()>,
+    neighbors_map:&HashMap<NodeIndex,HashSet<NodeIndex>>,
     current_clique: &mut HashSet<NodeIndex>,
     candidates: &mut HashSet<NodeIndex>,
     excluded: &mut HashSet<NodeIndex>,
@@ -30,22 +36,25 @@ fn bk_dfs(
     }
 
     // pick one pivot that have most degree, it will easy to cut branch
-    let pivot = candidates
-        .iter()
+    let pivot = candidates.iter()
         .chain(excluded.iter())
-        .max_by_key(|&n| graph.neighbors(*n).count())
+        .max_by_key(|&n| neighbors_map[n].len())
         .copied();
 
     // expand node that not neibour with pivot
     let remain;
     if let Some(p) = pivot {
         remain = candidates
-            .difference(&graph.neighbors(p).collect())
+            .difference(&neighbors_map[&p])
             .copied()
             .collect();
     } else {
         remain = candidates.clone();
     }
+
+    // sort the remain so that it can find max at first as possible
+    let mut remain:Vec<_> = remain.into_iter().collect();
+    remain.sort_by_key(|&n| Reverse(neighbors_map[&n].len()));
 
     for v in remain {
         let neighbors = graph.neighbors(v).collect();
@@ -56,6 +65,7 @@ fn bk_dfs(
         current_clique.insert(v);
         bk_dfs(
             graph,
+            neighbors_map,
             current_clique,
             &mut new_candidates,
             &mut new_excluded,
